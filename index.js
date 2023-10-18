a5ef09bbce951a235ce7578a1e8f77a0af3579c3
Sumit Pandey <spandey247789@gmail.com>
2023-10-18 16:18:43 +0530

    completed chat func

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-import express from "express"
+const express = require("express");
+const socket = require("socket.io");
 
 const app = express();
-app.listen(3000, () => console.log(`Listening on port 3000`))
+
+app.use(express.static("public"));
+
+const server = app.listen(3000, () => console.log(`Listening on port 3000`));
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("made socket connection", socket.id);
+
+  socket.on("message", (data) => {
+    io.sockets.emit("message", data);
+  });
+
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
+});
